13b5dcceaed80efa82d649096d52dbc0cd7b2937
Gajus Kuizinas <gajus@gajus.com>
2021-11-03 17:13:19 -0500

    chore: add generate-typescript-compatibility-rules script

--- bin/generate-typescript-compatibility-rules.js
+++ bin/generate-typescript-compatibility-rules.js
@@ -1,52 +1,35 @@
-const incompatibleRules = [
-  'brace-style',
-  'comma-dangle',
-  'comma-spacing',
-  'default-param-last',
-  'dot-notation',
-  'func-call-spacing',
-  'indent',
-  'init-declarations',
-  'keyword-spacing',
-  'lines-between-class-members',
-  'no-array-constructor',
-  'no-dupe-class-members',
-  'no-duplicate-imports',
-  'no-empty-function',
-  'no-extra-parens',
-  'no-extra-semi',
-  'no-implied-eval',
-  'no-invalid-this',
-  'no-loop-func',
-  'no-loss-of-precision',
-  'no-magic-numbers',
-  'no-redeclare',
-  'no-shadow',
-  'no-throw-literal',
-  'no-unused-expressions',
-  'no-unused-vars',
-  'no-use-before-define',
-  'no-useless-constructor',
-  'quotes',
-  'require-await',
-  'return-await',
-  'semi',
-  'space-before-function-paren',
-  'space-infix-ops',
-];
+const {
+  rules: typescriptRules,
+} = require('@typescript-eslint/eslint-plugin');
+const {
+  builtinRules,
+} = require('eslint/use-at-your-own-risk');
+
+const builtinRuleNames = Object.keys(Object.fromEntries(builtinRules));
+const typescriptRuleNames = Object.keys(typescriptRules);
+
+const incompatibleRuleNames = [];
+
+for (const typescriptRuleName of typescriptRuleNames) {
+  if (builtinRuleNames.includes(typescriptRuleName)) {
+    incompatibleRuleNames.push(typescriptRuleName);
+  }
+}
 
 const eslintConfiguration = require('../configurations/eslintrc.json');
 
 const appendRules = {};
 
-for (const incompatibleRule of incompatibleRules) {
-  if (!eslintConfiguration.rules[incompatibleRule] === undefined) {
+for (const incompatibleRuleName of incompatibleRuleNames) {
+  if (!eslintConfiguration.rules[incompatibleRuleName] === undefined) {
     continue;
   }
 
-  appendRules[incompatibleRule] = 0;
-  appendRules['@typescript-eslint/' + incompatibleRule] = eslintConfiguration.rules[incompatibleRule];
+  appendRules[incompatibleRuleName] = 0;
+  appendRules['@typescript-eslint/' + incompatibleRuleName] = eslintConfiguration.rules[incompatibleRuleName];
 }
 
+const orderedRules = Object.fromEntries(Object.entries(appendRules).sort());
+
 // eslint-disable-next-line no-console -- CLI
-console.log(JSON.stringify(appendRules));
+console.log(JSON.stringify(orderedRules, '', '  '));
