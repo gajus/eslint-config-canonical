463f281356d68951792a433d4ccf99c7afcca817
Gajus Kuizinas <gajus@gajus.com>
2021-11-11 12:37:34 -0600

    chore: add utility to identify deprecated rules
    This has been useful to catch a few improperly deprecated rules like:
    
    * https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/825
    * https://github.com/eslint/eslint/issues/15292

---
+++ compare/find-deprecated.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+
+/**
+ * https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/825
+ * https://github.com/eslint/eslint/issues/15292
+ */
+
+const got = require('got');
+const {
+  getLoadedRules,
+} = require('./utilities');
+
+(async () => {
+  const loadedRules = await getLoadedRules();
+
+  const ruleNames = Object.keys(loadedRules);
+
+  for (const ruleName of ruleNames) {
+    if (loadedRules[ruleName]?.meta?.deprecated) {
+      continue;
+    }
+
+    const ruleDocumentationUrl = loadedRules[ruleName]?.meta?.docs?.url;
+
+    if (!ruleDocumentationUrl) {
+      continue;
+    }
+
+    const response = await got(ruleDocumentationUrl, {
+      resolveBodyOnly: true,
+    });
+
+    if (response.toLowerCase().includes('deprecated')) {
+      console.warn('⚠️  ' + ruleName + ' suspected improperly deprecated rule (' + ruleDocumentationUrl + ')');
+    }
+  }
+})();
