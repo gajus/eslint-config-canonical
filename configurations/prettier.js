d308c5133f6a6494b628109ce2772bb1eacf189f
Gajus Kuizinas <gajus@gajus.com>
2023-01-06 19:07:22 -0600

    fix: disable overlapping rules

--- configurations/prettier.js
+++ configurations/prettier.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   rules: {
     'arrow-body-style': 0,
+    'prefer-arrow-callback': 0,
     'canonical/destructuring-property-newline': 0,
     'canonical/export-specifier-newline': 0,
     'canonical/import-specifier-newline': 0,
