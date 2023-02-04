a8c15bddc47afbb2a162cc1f1682c19576d7a0f8
Gajus Kuizinas <gajus@gajus.com>
2023-02-03 19:50:22 -0600

    fix: add missing import/ configuration

--- configurations/typescript.js
+++ configurations/typescript.js
@@ -202,7 +202,7 @@ module.exports = {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     'import/resolver': {
-      node: {
+      typescript: {
         extensions: typescriptExtensions,
       },
     },
