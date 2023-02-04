849cfccc061c68f0cf64759d349f424f998e3a3a
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2023-02-04 17:27:00 +0100

    fix: make `typescript-type-checking` config work by correcting typo

--- typescript-type-checking.js
+++ typescript-type-checking.js
@@ -1,9 +1,9 @@
 /* eslint-disable canonical/filename-match-exported */
 
-const typescript = require('./configurations/typescript-typed-linting');
+const typescript = require('./configurations/typescript-type-checking');
 
 typescript.rules = {
-  ...require('./configurations/typescript-typed-linting').rules,
+  ...require('./configurations/typescript-type-checking').rules,
 };
 
 module.exports = typescript;
