27b5a75ebd27822e6d68cbcd03a927b9ece29b06
Gajus <gajus@gajus.com>
2024-05-28 16:30:29 -0600

    fix: disable no-focused-tests fix

--- configurations/vitest.js
+++ configurations/vitest.js
@@ -7,7 +7,7 @@ module.exports.recommended = {
     'vitest/lower-case-title': 0,
     'vitest/max-nested-describe': 2,
     'vitest/no-conditional-tests': 2,
-    'vitest/no-focused-tests': 2,
+    'vitest/no-focused-tests': [2, { fixable: false }],
     'vitest/no-identical-title': 2,
     'vitest/no-skipped-tests': 2,
   },
