62f74d2446178a9bd40c837cb11340b3cea71c16
Gajus <gajus@gajus.com>
2024-07-16 17:02:12 -0600

    fix: re-enable import rules

--- compare/utilities.js
+++ compare/utilities.js
@@ -14,6 +14,7 @@ const getConfigurationPluginNames = async (configuration) => {
 };
 
 const getPluginRules = (pluginName) => {
+  // eslint-disable-next-line import/no-dynamic-require
   const { rules } = require(
     pluginName.startsWith('@')
       ? pluginName + '/eslint-plugin'
