ea49bb88f35d5183ab5ccc7ce57d3521f10699dd
Gajus <gajus@gajus.com>
2024-11-11 10:25:44 -0600

    fix: disable @stylistic/implicit-arrow-linebreak in prettier compat

--- configurations/prettier.js
+++ configurations/prettier.js
@@ -7,6 +7,7 @@ module.exports.recommended = {
     ...require('eslint-config-prettier').rules,
     '@stylistic/array-bracket-newline': 0,
     '@stylistic/array-element-newline': 0,
+    '@stylistic/implicit-arrow-linebreak': 0,
     '@stylistic/indent': 0,
     '@stylistic/line-comment-position': 0,
     '@stylistic/linebreak-style': 0,
