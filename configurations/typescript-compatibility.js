2b8c3496f7335cd946d923f0f3713f7d26a97c67
Gajus <gajus@gajus.com>
2024-11-11 09:14:29 -0600

    fix: add compat rules for block-spacing

--- configurations/typescript-compatibility.js
+++ configurations/typescript-compatibility.js
@@ -1,6 +1,10 @@
 module.exports.recommended = {
   files: ['**/*.{ts,tsx}'],
+  plugins: {
+    '@stylistic/ts': require('@stylistic/eslint-plugin-ts'),
+  },
   rules: {
+    '@stylistic/ts/brace-style': [2, 'always'],
     '@typescript-eslint/brace-style': [
       2,
       '1tbs',
@@ -26,8 +30,8 @@ module.exports.recommended = {
       },
     ],
     '@typescript-eslint/default-param-last': 2,
-
     '@typescript-eslint/func-call-spacing': [2, 'never'],
+
     '@typescript-eslint/indent': [2, 2],
     '@typescript-eslint/init-declarations': 0,
     '@typescript-eslint/keyword-spacing': [
@@ -41,8 +45,8 @@ module.exports.recommended = {
     '@typescript-eslint/no-dupe-class-members': 2,
     '@typescript-eslint/no-extra-parens': 2,
     '@typescript-eslint/no-extra-semi': 2,
-
     '@typescript-eslint/no-invalid-this': 0,
+
     '@typescript-eslint/no-loop-func': 2,
     '@typescript-eslint/no-loss-of-precision': 2,
     '@typescript-eslint/no-magic-numbers': 0,
@@ -60,8 +64,8 @@ module.exports.recommended = {
         hoist: 'all',
       },
     ],
-
     '@typescript-eslint/no-unused-expressions': 2,
+
     '@typescript-eslint/no-unused-vars': 2,
     '@typescript-eslint/no-use-before-define': [
       2,
@@ -79,6 +83,7 @@ module.exports.recommended = {
     '@typescript-eslint/space-before-blocks': [2, 'always'],
     '@typescript-eslint/space-before-function-paren': [2, 'always'],
     '@typescript-eslint/space-infix-ops': 2,
+    'block-spacing': 0,
     'brace-style': 0,
     'comma-dangle': 0,
     'comma-spacing': 0,
