504e55ca39a9dda772b0c1f4992859d3912270a1
Gajus <gajus@gajus.com>
2024-11-11 11:11:35 -0600

    fix: prettier compat

--- configurations/prettier.js
+++ configurations/prettier.js
@@ -7,6 +7,7 @@ module.exports.recommended = {
     ...require('eslint-config-prettier').rules,
     '@stylistic/array-bracket-newline': 0,
     '@stylistic/array-element-newline': 0,
+    '@stylistic/comma-dangle': 0,
     '@stylistic/implicit-arrow-linebreak': 0,
     '@stylistic/indent': 0,
     '@stylistic/jsx-quotes': 0,
