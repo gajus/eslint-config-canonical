1c7c5e3820f0600b2c3ee2cd492caa17f26edbd7
Gajus <gajus@gajus.com>
2025-11-04 13:50:01 -0600

    feat: expose react hooks

--- configurations/index.ts
+++ configurations/index.ts
@@ -12,6 +12,7 @@ export * as moduleRules from './module.js';
 export * as next from './next.js';
 export * as node from './node.js';
 export * as prettier from './prettier.js';
+export * as reactHooks from './react-hooks.js';
 export * as react from './react.js';
 export * as regexp from './regexp.js';
 export * as typescript from './typescript.js';
