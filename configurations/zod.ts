c061b5909f476d143573b93a5b3977bf9085cbde
Gajus <gajus@gajus.com>
2026-01-26 04:58:08 -0600

    feat: update eslint-plugin-zod

--- configurations/zod.ts
+++ configurations/zod.ts
@@ -3,11 +3,22 @@ import { defineConfig } from 'eslint/config';
 
 export const recommended = defineConfig({
   plugins: {
-    // @ts-expect-error - not properly typed
     zod: zodPlugin,
   },
   rules: {
-    'zod/prefer-enum': 2,
-    'zod/require-strict': 2,
+    'zod/array-style': [2, { style: 'function' }],
+    'zod/no-number-schema-with-int': 2,
+    'zod/no-optional-and-default-together': 2,
+    'zod/require-schema-suffix': [2, { suffix: 'ZodSchema' }],
+    'zod/no-any-schema': 2,
+    'zod/prefer-meta': 2,
+    'zod/prefer-meta-last': 2,
+    'zod/prefer-enum-over-literal-union': 2,
+    'zod/consistent-object-schema-type': [
+      2,
+      {
+        allow: ['strictObject'],
+      },
+    ],
   },
 });
